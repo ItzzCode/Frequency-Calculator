f057d61d8e48847dca50fa718a18d27fd2174814
ItzzCode <51547168+ItzzCode@users.noreply.github.com>
2024-10-23 21:32:46 +0000

    Input Mode

--- main.py
+++ main.py
@@ -20,17 +20,16 @@ def cutOffFrequency(avgHoleRadius: float, \
                     avgHoleLength: float) -> float:
     return 0.11 * (avgHoleRadius / boreRadius) * (speedOfSound / math.sqrt(halfAvgHoleDistance * avgHoleLength))
 
+inputMode = input("Use default values? (Y/n): ").lower() == "n"
+
 tubeRadius: float = 1.5 / 100 #m, don't set to wide lest cutOffFrequency() errors
 tubeOuterWidth: float = 1 / 100 #m
 speedOfSound: float = 343.7 #m/s
 A: int = 220 #Hz
-lowestFrequency: float = A*2**(8/12) #Hz, set to F
-tubeLength: float = frequencyToLength(lowestFrequency) - 2*endCorrection(tubeRadius)
-
+stepsFromA: int = 8 #set to F
 noteIntervals: list = [2, 4, 5, 7, 9, 11, 12] #14, 16, 17, 19]
 
-#goes from furthest to closest from place of articulation
-holePlacements: list = []
+#holeRadii, Placements, & Frequencies are in furthest to closest order from place of articulation
 holeRadii: list = [ 
     0.1,
     0.1,
@@ -40,10 +39,49 @@ holeRadii: list = [
     0.1,
     0.1,
     0.1
-]; 
+];
+
+if inputMode:
+    anInput = input(f"Enter the radius of the tube (cm, default: {tubeRadius*100:.0f}): ")
+    if anInput != "":
+        tubeRadius = float(anInput) / 100
+    
+    anInput = input(f"Enter the outer width of the tube (cm, default: {tubeOuterWidth*100:.0f}): ")
+    if anInput != "":
+        tubeOuterWidth = float(anInput) / 100
+
+    anInput = input(f"Enter the speed of sound (m/s, default: {speedOfSound}): ")
+    if anInput != "":
+        speedOfSound = float(anInput)
+
+    anInput = input(f"Enter the frequency of concert A (Hz, default: {A}): ")
+    if anInput != "":
+        A = int(anInput)
+
+    anInput = input(f"Enter the number of steps from A (default: {stepsFromA}): ")
+    if anInput != "":
+        stepsFromA = int(anInput)
+
+    anInput = input(f"Enter the number of holes (defaults: \n\t\
+        Holes: {len(noteIntervals)}\n\t\
+        Steps from fundamental: {noteIntervals}\n\t\
+        Hole radii: {holeRadii}\n): ")
+    if anInput != "":
+        noteIntervals = []
+        holeRadii = []
+        for i in range(int(anInput)):
+            anInput = input(f"Enter the radius of hole {i+1} (cm): ")
+            holeRadii.append(float(anInput) / 100)
+            
+            anInput = input(f"Enter the steps from fundamental of hole {i+1}: ")
+            noteIntervals.append(int(anInput))
+
+lowestFrequency: float = A * 2**(8/12) #Hz
+tubeLength: float = frequencyToLength(lowestFrequency) - 2 * endCorrection(tubeRadius)
+holePlacements: list = []
 holeFrequencies: list = []
 
-#(N * [''])[:N]
+print()
 
 for i, interval in enumerate(noteIntervals):
     note = lowestFrequency * 2**(interval/12)
@@ -58,6 +96,7 @@ for i in range(len(holePlacements)-1):
 
 cutOff = cutOffFrequency(avg(holeRadii[:len(holeDistances)-1]), tubeRadius, speedOfSound, avg(holeDistances)/2, avg(holePlacements))
 
+print("F - Fundamental; C - Cut-Off Frequency")
 for i, holePlacement in enumerate(holePlacements[::-1]):
     print(f"{i+1}: {holePlacement*100:5.2f}cm{holeFrequencies[::-1][i]:>9.2f}Hz")
 print(f"F: {tubeLength*100:5.2f}cm{lowestFrequency:>9.2f}Hz")
